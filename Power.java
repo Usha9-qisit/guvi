2a55eb985b2508d3a636ef6dd138185c27a0f16c
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-07-27 12:51:43 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Power
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int p=s.nextInt();
+		int result=1;
+		while(p!=0)
+		{
+			result*=n;
+			--p;
+		}
+		System.out.println(result);
+	}
+	}
+}

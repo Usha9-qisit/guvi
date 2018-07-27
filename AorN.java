e3d7cb01ae1849d3f16311cde8f9e2eccd0f7ed3
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-07-27 10:52:49 +0530

    Update AorN.java

--- AorN.java
+++ AorN.java
@@ -1,13 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 import java.util.Scanner;
-class AorN {
-
-    public static void main(String[] args) {
-
-        Scanner sc=new Scanner(System.in);
-	char ch=sc.next( ).charAt(0);	 		
-  if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-            System.out.println("Alphabet.");
-        else
-            System.out.println("Not");
-    }
+/* Name of the class has to be "Main" only if the class is public. */
+class AorN
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	char ch=sc.next().charAt(0);
+	if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch >= 'Z' ))
+	System.out.println("Alphabet");
+	else
+	System.out.println("Not");
+	}
 }

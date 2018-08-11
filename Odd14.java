e4bd3c2b82ee02299a3e6d119b11571834bc4217
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-11 10:38:04 +0530

    Create Odd14.java

---
+++ Odd14.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Odd14
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		for(int i=n1+1;i<n2;i++)
+		if(i%2!=0)
+		{System.out.println(i);}
+	}
+}

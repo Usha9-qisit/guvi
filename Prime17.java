fea50cf42d22a61e26048e814b313107db6b09cd
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-11 12:27:07 +0530

    Create Prime17.java

---
+++ Prime17.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Prime17
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		for(int i=a+1;i<b;i++)
+	    {
+	    	int counter=0;
+	    	for(int num=1;num>=1;num--)
+	    	{
+	    		counter=counter+1;
+	    	}
+	    	if(counter==2)
+	    	{
+	    		System.out.println(i);
+	    	}
+	    }
+	}
+}

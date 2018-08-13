5be15b2771f9befc570bf14804bf2b52af020920
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 10:03:59 +0530

    Update and rename Fact19.java to Fa19.java

---
+++ Fa19.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Fa19
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int i,fact=1;
+	 Scanner s=new Scanner(System.in);
+	 int n=s.nextInt();
+	 for(i=1;i<=n;i++)
+	 {
+	 	fact=fact*i;
+	 }
+	 System.out.println(fact);
+	}
+}

5bc1173480ec2c953d3e5117b1a57128cbbcce55
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 20:06:17 +0530

    Create Sen32.java

---
+++ Sen32.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Sen32
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)==' ')
+			{
+				count++;
+			}
+		}
+		System.out.println(count+1);
+	}
+}

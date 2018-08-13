36f3b43f5d8ca414ccfbe58ad56e6b6b45967f14
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 19:59:55 +0530

    Update Sen31.java

--- Sen31.java
+++ Sen31.java
@@ -1,20 +1,20 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class Sen31
+class Sen13
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
 		String str=s.nextLine();
-		char[] ch = str.toCharArray();
-		int letter=0;
-		for(int i = 0; i < str.length(); i++)
-			if(Character.isLetter(ch[i]))
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)!=' ')
 			{
-				letter ++ ;
+				count++;
 			}
-				System.out.println(letter);
-			
+		}
+		System.out.println(count);
 	}
 }

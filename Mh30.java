64f445ab02757d54cb524927465a1bba4bb1352e
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 19:30:01 +0530

    Create Mh30.java

---
+++ Mh30.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Mh30
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int hr1=s.nextInt();
+		int min1=s.nextInt();
+		int hr2=s.nextInt();
+		int min2=s.nextInt();
+		System.out.println(Math.abs(hr1-hr2)+" "+Math.abs(min1-min2));
+	}
+}

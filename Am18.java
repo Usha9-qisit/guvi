083d87cf3c12fa37f8b6c07fc92b963f880564a0
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 09:43:47 +0530

    Create Am18.java

---
+++ Am18.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Am18
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n, n1, n2, i, rem, temp;
+        Scanner scan = new Scanner(System.in);
+        n1 = scan.nextInt();
+        n2 = scan.nextInt();
+	 for(i=n1+1; i<n2; i++)
+        {
+            temp = i;
+            n = 0;
+            while(temp != 0)
+            {
+                rem = temp%10;
+                n = n + rem*rem*rem;
+                temp = temp/10;
+            }
+            if(i == n)
+            {
+              System.out.print(i + "  ");
+              }
+        }
+       
+    
+	}
+}

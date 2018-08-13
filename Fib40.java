97158638c91bfbdb991192e06312693b99de643f
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 22:20:04 +0530

    Create Fib40.java

---
+++ Fib40.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Fib40
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a=0,b=1;
+		 for (int i = 1; i <= n; ++i)
+        {
+            int sum = a+b;
+            a=b;
+            b=sum;
+        System.out.printf(a+" ");
+        }
+    
+	}
+}

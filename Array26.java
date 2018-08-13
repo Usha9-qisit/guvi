648d4b7a83f9c043cb71145f9e5c07b884e2b94a
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 12:58:11 +0530

    Create Array26.java

---
+++ Array26.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.Arrays;
+import java.lang.*;
+import java.io.*;
+class Array26
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+		  a[i]=s.nextInt();
+		}
+	           Arrays.sort(a);
+		for(i=0;i<n;i++)
+		{
+			if(i==n-1)
+			{
+				System.out.print(a[i]);
+			}
+			else
+			{
+				System.out.print(a[i]+" ");
+			}
+		}
+		
+		
+	}
+}

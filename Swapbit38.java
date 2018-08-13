672a2e2219828af58712150b48bfc474a92b05b4
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 21:57:09 +0530

    Create Swapbit38.java

---
+++ Swapbit38.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Swapbit38
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println(a+" "+b);
+	}
+}

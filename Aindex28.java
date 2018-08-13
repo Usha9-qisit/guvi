bb540f1433a5e3298070cef0043ee4f47dbac149
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 14:00:21 +0530

    Create Aindex28.java

---
+++ Aindex28.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Aindex28
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+		  a[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+	
+		System.out.println(a[i]+" "+i);
+			
+		}
+	}
+}

793cd7d932553f5b6a9ae96ae873fc04b9985d3f
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-13 10:24:24 +0530

    Create Mu20.java

---
+++ Mu20.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Mu20
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(n*i+" ");
+		}
+	}
+}

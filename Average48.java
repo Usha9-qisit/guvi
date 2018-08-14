0687c8837f72fb3a0216ff0e61a72d849d909f29
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-14 12:45:33 +0530

    Create Average48.java

---
+++ Average48.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Average48
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner s=new Scanner(System.in);
+	  int n=s.nextInt();
+	  int a[]=new int[n];
+	  for(int i=0;i<n;i++)
+	  {
+	  	a[i]=s.nextInt();
+	  }
+	  int sum=0;
+	  for(int i=0;i<n;i++)
+	  {
+	  	sum=sum+a[i];
+	  }
+	  int average=sum/n;
+	  System.out.println(average);
+            }
+	
+}

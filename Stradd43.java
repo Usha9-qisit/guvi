86eb9807654e3ae69868521fb95671fdc8111860
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-14 12:01:47 +0530

    Create Stradd43.java

---
+++ Stradd43.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Stradd43
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str1, str2,str3;
+                       Scanner in = new Scanner(System.in);
+                       str1 = in.next();
+                       str2 = in.next();
+                       str3=str1+str2;
+                       System.out.println(str3);
+                      
+	}
+}

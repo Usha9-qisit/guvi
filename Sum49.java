be38eba35421677498f8edd7e1fdb8131bdbecf2
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-14 12:49:30 +0530

    Create Sum49.java

---
+++ Sum49.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Sum49
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner s=new Scanner(System.in);
+	  int n1=s.nextInt();
+	  int n2=s.nextInt();
+	  int n3;
+	  n3=n1+n2;
+	  System.out.println(n3);
+	  }
+	
+}

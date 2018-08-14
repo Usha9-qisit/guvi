092a42aed9354c877353f039212f338b7a4d1eb9
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-14 11:36:26 +0530

    Create String41.java

---
+++ String41.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class String41
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		int k=s.nextInt();
+		for(int i=0;i<k;i++)
+		System.out.println(str);
+	}
+}

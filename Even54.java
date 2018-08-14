424908e8c6b373ca0000971adfe0fb7b9c701f1a
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-14 16:50:18 +0530

    Update and rename Eodd54.java to Even54.java

---
+++ Even54.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Even54
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%2==0)
+		
+			System.out.println(n);
+		else
+			System.out.println(n-1);
+	}
+}

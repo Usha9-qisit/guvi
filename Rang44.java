607f212b3ab62b7338705ac4d423e3858e9d5eb3
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-14 12:11:25 +0530

    Create Rang44.java

---
+++ Rang44.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Rang44
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner s=new Scanner(System.in);
+	  int n=s.nextInt();
+	  if(n>=1 && n<=10)
+	  System.out.println("yes");
+	  else
+	  System.out.println("no");
+	}
+}

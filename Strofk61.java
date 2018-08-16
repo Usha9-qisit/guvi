0c60085867ee59637b38b29866c820ce37f89a58
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-16 11:27:13 +0530

    Create Strofk61.java

---
+++ Strofk61.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Strofk61
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		int k=s.nextInt();
+		for(int i=0;i<k;i++)
+		{
+		 System.out.print(str.charAt(i));
+		}
+		
+	}
+}

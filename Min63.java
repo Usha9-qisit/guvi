f88cb931aded3d802c110c12bf3038a54a1377ea
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-16 11:39:54 +0530

    Create Min63.java

---
+++ Min63.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Min63
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a[]=new int[10];
+		for(int i=0;i<10;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		int min=a[0];
+		for(int i=0;i<10;i++)
+		{
+			if(min>a[i])
+			min=a[i];
+		}
+		System.out.println(min);
+	}
+}

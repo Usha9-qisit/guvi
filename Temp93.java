716aca79a9927a44a3b6060e3c0957d7566b1ef1
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-17 12:29:57 +0530

    Create Temp93.java

---
+++ Temp93.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Temp93
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int res;
+		res=n+273;
+		System.out.println(res);
+	}
+}

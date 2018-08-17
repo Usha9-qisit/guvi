d1857d6b020f5d1fb3569304f012341cef595a96
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-17 12:45:37 +0530

    Create Mulsev68.java

---
+++ Mulsev68.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Mulsev68
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%7==0)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}

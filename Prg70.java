57608fca67e8864340082d9db476022cc6316c3b
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-17 13:05:02 +0530

    Create Prg70.java

---
+++ Prg70.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Prg70
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int res;
+		res=a*2;
+		System.out.println(res);
+		
+	}
+}

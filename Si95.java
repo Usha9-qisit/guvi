7ae55e38f36408cc0f53138183207ed48acbb94b
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-17 11:11:59 +0530

    Create Si95.java

---
+++ Si95.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Si95
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	int P=s.nextInt();
+	int T=s.nextInt();
+	int R=s.nextInt();
+	int res;
+	res=(P*T*R)/100;
+	System.out.println(res);
+	}
+	
+}

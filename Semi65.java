cd11c159440df209e09dd106aae2f40e5aa51227
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-17 09:42:43 +0530

    Create Semi65.java

---
+++ Semi65.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Semi65
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		if(System.out.printf(str)==null){}
+	}
+}

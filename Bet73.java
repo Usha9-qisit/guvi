e80c513efc4e2e8f4d3133ec4ea7b7d8a8280615
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-21 10:30:51 +0530

    Update and rename Prg73.java to Bet73.java

---
+++ Bet73.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Bet73
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int l=s.nextInt();
+		int r=s.nextInt();
+		if(n>l || n<r)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}

941253206057ccbac2e46639d654cdb0a925640c
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-21 10:42:43 +0530

    Create Prog74.java

---
+++ Prog74.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Prog74
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+	            double a=s.nextDouble();
+		System.out.println((int)Math.ceil(a));
+		
+	}
+}

2806ea8043d39685a81f04fdb331cb9e798e5d58
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-22 11:35:08 +0530

    Create Proascii84.java

---
+++ Proascii84.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Proascii84
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		int t=(int)ch;
+		System.out.println(t);
+	
+	}
+}

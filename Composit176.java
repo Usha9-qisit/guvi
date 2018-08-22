9a7e904733b11b5c055620f1deaa9480b43fbd3a
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-22 10:28:22 +0530

    Create Composit176.java

---
+++ Composit176.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Composit176
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		boolean flag=false;
+		for(int i=2;i<n/2;i++)
+		{
+			if(n%i==0)
+			{
+			flag=true;
+			break;
+			}
+			else
+			flag=false;
+
+		}
+		if(flag==true)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	
+
+	}
+}

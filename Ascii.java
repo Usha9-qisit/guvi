31bf6a87bed8c2e045216a4cbe1757cb2b71b803
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-22 11:36:40 +0530

    Update and rename Proascii84.java to Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ascii
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		int t=(int)ch;
+		System.out.println(t);
+	
+	}
+}

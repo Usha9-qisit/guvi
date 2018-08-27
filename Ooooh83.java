f589db2c5309290592dd0e51a2c5e80233aefa6a
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-27 14:52:25 +0530

    Create Ooooh83.java

---
+++ Ooooh83.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ooooh83
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		char ch=s.next().charAt(0);
+	            int b=s.nextInt();
+		if(ch == '/')
+		System.out.println(a/b);
+		else
+		System.out.println(a%b);
+	}
+}

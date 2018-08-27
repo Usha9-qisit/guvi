412d553910422e21b7fb162eaf0cd31a389c2120
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-27 10:44:30 +0530

    Create Gcd87.java

---
+++ Gcd87.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Gcd87
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int gcd=1;
+		for(int i=1;i<=n && i<=m;++i)
+		{
+			if(n%i==0 &&m%i==0)
+			gcd=i;
+		}
+		System.out.println(gcd);
+		
+	}
+}

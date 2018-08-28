1609e3181921d3bd304b17f0b6e545c8a8de09b1
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-28 10:40:14 +0530

    Create Iso86.java

---
+++ Iso86.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Iso86
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		isIsogram(str);
+	}
+	static void isIsogram(String s)
+	{
+		String[] ary=s.split("");
+		Set<String> mySet=new HashSet<String>(Arrays.asList(ary));
+		if(s.length() == mySet.size())
+		{
+			System.out.println("Yes");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+	}
+}

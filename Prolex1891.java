85c969a55a8380ad31aca4f3a0661ffaca693ff2
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-28 10:51:41 +0530

    Update and rename Lexico889.java to Prolex1891.java

---
+++ Prolex1891.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Prolex1891
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		char str1[]=str.toCharArray();
+		Arrays.sort(str1);
+		System.out.println(str1);
+	
+	}
+}

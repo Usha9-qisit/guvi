e526ec4885d3f6ce9883a3413b51adca4b42f99c
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-08-28 10:55:09 +0530

    Create Ideone90.java

---
+++ Ideone90.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone90
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		for(int i=0;i<s.length();i++)
+		{
+			if(Character.isDigit(s.charAt(i)))
+			System.out.print(s.charAt(i));
+		}
+	}
+}

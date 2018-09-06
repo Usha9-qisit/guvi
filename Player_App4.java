60e54d48c2b854297613ac7778f0c7c6380f462e
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-09-06 12:35:11 +0530

    Create Player_App4.java

---
+++ Player_App4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Player_App4
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner s=new Scanner(System.in);
+	   String str1=s.nextLine();
+	   String str2="";
+     StringBuilder sb = new StringBuilder(str1);
+     str2= sb.append(".").toString(); 
+     System.out.println(str2); 
+
+	}
+}

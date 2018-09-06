3e0537fd860cb1e2ccc2c8c8452fbb7fa621f6f4
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-09-06 13:01:30 +0530

    Create Player_Sub43.java

---
+++ Player_Sub43.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Player_Sub43
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+            	Scanner s=new Scanner(System.in);
+	    String str=s.next();
+	    String str1=s.next();
+	    boolean isFound=str.indexOf(str1)!=-1?true:false;
+	    if(isFound)
+	    System.out.println("yes");
+	    else
+	    System.out.println("no");
+		
+	}
+}

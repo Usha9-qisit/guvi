551e90fdfe83f358647c2d213fc0d6f7b2548107
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-09-10 11:25:47 +0530

    Update and rename BEG01080.java to AK.java

---
+++ AK.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class AK
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=1;i<n;i++)
+		a[i]=sc.nextInt();
+		System.out.println(a[k]);
+		
+	}
+}

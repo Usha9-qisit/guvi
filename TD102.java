4664e2b8445a6f7dc04303cabf34c2b7a82a2a94
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-09-10 10:59:18 +0530

    Create TD102.java

---
+++ TD102.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class TD102
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		if(n1%2==0)
+		{
+			System.out.println(n1/2);
+		}
+		else
+		{
+			System.out.println(n1);
+		}
+	}
+}

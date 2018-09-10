647d7c618696b679c367d75408df72866e0bb58e
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-09-10 11:02:22 +0530

    Create PRG104.java

---
+++ PRG104.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class PRG104
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		System.out.println((int)Math.pow(n,k));
+
+	}
+}

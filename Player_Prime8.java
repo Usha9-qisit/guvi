9d488adb120aab934d74d980dfb6679c067bf6fc
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-09-10 10:31:52 +0530

    Create Player_Prime8.java

---
+++ Player_Prime8.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Player_Prime8
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int f=0;
+		Scanner sc=new Scanner (System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=0;
+		for(int j=a;j<=b;j++)
+		{
+			f=0;
+		for(int i=2;i<j;i++)
+		{
+			if(j%i==0)
+			{
+				f=1;
+				break;	
+			}
+			
+		}
+			if(f==0)
+			{
+				c++;
+			}
+		}
+		
+		System.out.println(c);
+	}
+}

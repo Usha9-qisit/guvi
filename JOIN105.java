c3a0f201318505d20d997d66d40d43ae51e46d5c
Usha Sree.Punati <41729974+Usha9-qisit@users.noreply.github.com>
2018-09-10 11:07:39 +0530

    Create JOIN105.java

---
+++ JOIN105.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class JOIN105
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		int j=sc.nextInt();
+		String k=""+i;
+		k+=j;
+		System.out.println(k);
+	}
+}
